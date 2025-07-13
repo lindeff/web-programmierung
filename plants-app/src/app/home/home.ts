8dd12c8c0d50ffcca07e0180d27364ae65ed639c
mige <linmige@hotmail.com>
2025-07-13 15:07:30 +0200

    add more highlights

--- plants-app/src/app/home/home.ts
+++ plants-app/src/app/home/home.ts
@@ -10,4 +10,28 @@ import { Header } from '../components/header/header';
   styleUrl: './home.css'
 })
 export class Home {
+  anthuriumCards = [
+    {
+      name: 'Anthurium BVEP/Antolakii',
+      price: 'NFS',
+      coverImageUrl: 'assets/cover-bvep.png',
+      hoverImageUrl: 'assets/bvep.jpg',
+      gallery: [
+        { href: 'assets/bvep.jpg', title: 'Main view' },
+        { href: 'assets/background-house.jpg', title: 'Side view' },
+      ]
+    },
+    {
+      name: 'Anthurium Mr. Worldwide',
+      price: 'NFS',
+      coverImageUrl: 'assets/cover-mr-worldwide.png',
+      hoverImageUrl: 'assets/mr-worldwide.jpg'
+    },
+    {
+      name: 'Anthurium DocBlock Zara \'29\' x (Michelle x Zara) \'28\'',
+      price: 'NFS',
+      coverImageUrl: 'assets/zamiza.png',
+      hoverImageUrl: 'assets/zamiza.jpg'
+    }
+  ];
 }
